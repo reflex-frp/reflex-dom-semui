b2bd1280466f58568a74b09abb1a5a73ad3b6733
Doug Beardsley <mightybyte@gmail.com>
2016-10-05 23:56:31 -0700

    More button functionality including an variant that has more structure

--- src/Reflex/Dom/SemanticUI/Button.hs
+++ src/Reflex/Dom/SemanticUI/Button.hs
@@ -15,38 +15,31 @@
 module Reflex.Dom.SemanticUI.Button where
 
 ------------------------------------------------------------------------------
-import           Control.Monad
-import           Control.Monad.Trans
 import           Data.Default
-import           Data.Map (Map)
-import qualified Data.Map as M
 import           Data.Maybe
-import           Data.Monoid
 import           Data.Text (Text)
 import qualified Data.Text as T
-import           GHCJS.DOM.Types hiding (Event, Text)
-#ifdef ghcjs_HOST_OS
-import           GHCJS.Foreign.Callback
-import           GHCJS.Foreign
-import           GHCJS.Marshal.Internal
-import           GHCJS.Types
-#endif
-import           Reflex
---import           Reflex.Host.Class
 import           Reflex.Dom hiding (fromJSString)
 ------------------------------------------------------------------------------
 import           Reflex.Dom.SemanticUI.Common
 ------------------------------------------------------------------------------
 
 data UiButton = UiButton
-    { _uiButton_emphasis :: Maybe UiEmphasis
-    , _uiButton_color    :: Maybe UiColor
-    , _uiButton_basic    :: Maybe UiBasic
-    , _uiButton_inverted :: Maybe UiInverted
+    { _uiButton_emphasis   :: Maybe UiEmphasis
+    , _uiButton_color      :: Maybe UiColor
+    , _uiButton_basic      :: Maybe UiBasic
+    , _uiButton_inverted   :: Maybe UiInverted
+    , _uiButton_activation :: Maybe UiActivation
+    , _uiButton_size       :: Maybe UiSize
+    , _uiButton_loading    :: Maybe UiLoading
+    , _uiButton_compact    :: Maybe UiCompact
+    , _uiButton_toggle     :: Maybe UiToggle
+    , _uiButton_fluid      :: Maybe UiFluid
+    , _uiButton_circular   :: Maybe UiCircular
     } deriving (Eq,Show)
 
 instance Default UiButton where
-  def = UiButton def def def def
+  def = UiButton def def def def def def def def def def def
 
 instance UiHasColor UiButton where
   uiSetColor c b = b { _uiButton_color = Just c }
@@ -60,13 +53,64 @@ instance UiHasBasic UiButton where
 instance UiHasInverted UiButton where
   inverted b = b { _uiButton_inverted = Just UiInverted }
 
-uiButton :: MonadWidget t m => UiButton -> m a -> m a
-uiButton UiButton{..} children = do
-    elClass "button" (T.unwords ["ui", attrs, "button"]) children
+instance UiHasActivation UiButton where
+  uiSetActivation a b = b { _uiButton_activation = Just a }
+
+instance UiHasSize UiButton where
+  uiSetSize c b = b { _uiButton_size = Just c }
+
+instance UiHasLoading UiButton where
+  loading b = b { _uiButton_loading = Just UiLoading }
+
+instance UiHasCompact UiButton where
+  compact b = b { _uiButton_compact = Just UiCompact }
+
+instance UiHasToggle UiButton where
+  toggle b = b { _uiButton_toggle = Just UiToggle }
+
+instance UiHasFluid UiButton where
+  fluid b = b { _uiButton_fluid = Just UiFluid }
+
+instance UiHasCircular UiButton where
+  circular b = b { _uiButton_circular = Just UiCircular }
+
+uiButtonAttrs :: UiButton -> Text
+uiButtonAttrs UiButton{..} = T.unwords $ catMaybes
+    [ uiText <$> _uiButton_emphasis
+    , uiText <$> _uiButton_color
+    , uiText <$> _uiButton_basic
+    , uiText <$> _uiButton_inverted
+    , uiText <$> _uiButton_activation
+    , uiText <$> _uiButton_size
+    , uiText <$> _uiButton_loading
+    , uiText <$> _uiButton_compact
+    , uiText <$> _uiButton_toggle
+    , uiText <$> _uiButton_fluid
+    , uiText <$> _uiButton_circular
+    ]
+
+uiButton :: MonadWidget t m => Dynamic t UiButton -> m a -> m a
+uiButton bDyn children = do
+    elDynAttr "button" (mkAttrs <$> bDyn) children
   where
-    attrs = T.unwords $ catMaybes
-              [ uiText <$> _uiButton_emphasis
-              , uiText <$> _uiButton_color
-              , uiText <$> _uiButton_basic
-              , uiText <$> _uiButton_inverted
-              ]
+    mkAttrs b = "class" =: T.unwords ["ui", uiButtonAttrs b, "button"]
+
+data UiButtonAnimationType
+  = HorizontalAnimation
+  | VerticalAnimation
+  | FadeAnimation
+  deriving (Eq,Ord,Read,Show,Enum,Bounded)
+
+instance UiClassText UiButtonAnimationType where
+   uiText HorizontalAnimation = "animated"
+   uiText VerticalAnimation = "vertical animated"
+   uiText FadeAnimation = "animated fade"
+
+uiButtonAnimated :: MonadWidget t m => UiButtonAnimationType -> Dynamic t UiButton -> m a -> m a -> m a
+uiButtonAnimated anim bDyn visible hidden = do
+    elDynAttr "button" (mkAttrs <$> bDyn) $ do
+      divClass "visible content" visible
+      divClass "hidden content" hidden
+  where
+    mkAttrs b = "class" =: T.unwords ["ui", uiButtonAttrs b, uiText anim, "button"]
+
--- src/Reflex/Dom/SemanticUI/Common.hs
+++ src/Reflex/Dom/SemanticUI/Common.hs
@@ -144,3 +144,95 @@ instance UiClassText UiActivation where
   uiText UiActive = "active"
   uiText UiDisabled = "disabled"
 
+class UiHasActivation a where
+  uiSetActivation :: UiActivation -> a -> a
+
+active, disabled :: UiHasActivation a => a -> a
+active = uiSetActivation UiActive
+disabled = uiSetActivation UiDisabled
+
+------------------------------------------------------------------------------
+data UiSize
+  = UiMini
+  | UiTiny
+  | UiSmall
+  | UiMedium
+  | UiLarge
+  | UiBig
+  | UiHuge
+  | UiMassive
+  deriving (Eq,Ord,Read,Show,Enum,Bounded)
+
+instance UiClassText UiSize where
+  uiText UiMini = "mini"
+  uiText UiTiny = "tiny"
+  uiText UiSmall = "small"
+  uiText UiMedium = "medium"
+  uiText UiLarge = "large"
+  uiText UiBig = "big"
+  uiText UiHuge = "huge"
+  uiText UiMassive = "massive"
+
+class UiHasSize a where
+  uiSetSize :: UiSize -> a -> a
+
+mini, tiny, small, medium, large, big, huge, massive :: UiHasSize a => a -> a
+mini = uiSetSize UiMini
+tiny = uiSetSize UiTiny
+small = uiSetSize UiSmall
+medium = uiSetSize UiMedium
+large = uiSetSize UiLarge
+big = uiSetSize UiBig
+huge = uiSetSize UiHuge
+massive = uiSetSize UiMassive
+
+------------------------------------------------------------------------------
+data UiLoading = UiLoading
+  deriving (Eq,Ord,Read,Show,Enum,Bounded)
+
+instance UiClassText UiLoading where
+  uiText UiLoading = "loading"
+
+class UiHasLoading a where
+  loading :: a -> a
+
+------------------------------------------------------------------------------
+data UiCompact = UiCompact
+  deriving (Eq,Ord,Read,Show,Enum,Bounded)
+
+instance UiClassText UiCompact where
+  uiText UiCompact = "compact"
+
+class UiHasCompact a where
+  compact :: a -> a
+
+------------------------------------------------------------------------------
+data UiToggle = UiToggle
+  deriving (Eq,Ord,Read,Show,Enum,Bounded)
+
+instance UiClassText UiToggle where
+  uiText UiToggle = "toggle"
+
+class UiHasToggle a where
+  toggle :: a -> a
+
+------------------------------------------------------------------------------
+data UiFluid = UiFluid
+  deriving (Eq,Ord,Read,Show,Enum,Bounded)
+
+instance UiClassText UiFluid where
+  uiText UiFluid = "fluid"
+
+class UiHasFluid a where
+  fluid :: a -> a
+
+
+------------------------------------------------------------------------------
+data UiCircular = UiCircular
+  deriving (Eq,Ord,Read,Show,Enum,Bounded)
+
+instance UiClassText UiCircular where
+  uiText UiCircular = "circular"
+
+class UiHasCircular a where
+  circular :: a -> a
